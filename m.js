d46fc68bcacde06a462eaf3dc30dd371880d57bc
first <puisilu71@gmail.com>
2024-07-29 21:29:40 +0530

    add essential files

---
+++ m.js
@@ -0,0 +1,9 @@
+function sum(nums){
+    var result=0;
+    for(var i=0;i<=nums.length-1;i++){
+        result+=nums[i];
+    }
+    return result;
+}
+var numbers =[1,2,3,4,5]
+console.log(sum(numbers))
