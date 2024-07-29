a49a2232b49be2f2ba9cea4b8a2612829adf9e33
Bimalendu Bhaskar Pui <94634268+Puisilu@users.noreply.github.com>
2024-07-29 21:34:42 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,9 +1,37 @@
-function sum(nums){
-    var result=0;
-    for(var i=0;i<=nums.length-1;i++){
-        result+=nums[i];
-    }
-    return result;
-}
-var numbers =[1,2,3,4,5]
-console.log(sum(numbers))
+let string="";
+let buttons=document.querySelectorAll('button')
+    Array.from(buttons).forEach( (btn)=>
+
+    {
+        btn.addEventListener('click',(e)=>
+        {
+            console.log(e.target)
+            if(e.target.innerHTML=="=")
+                {
+string=eval(string)
+document.querySelector('#t').value=string
+                }
+               else if(e.target.innerHTML=="c")
+                    {
+    string=""
+    document.querySelector('#t').value=string
+                    }
+                else{
+string=string + e.target.innerHTML
+document.querySelector('#t').value=string}
+                
+        })
+    })
+
+
+    let str="hello";
+   
+    
+    console.log(str.split("").reverse().join(""))
+
+
+    const arr=[1,2,3,4,5,6,5,4,1,3,4,6,7,9,8]
+    
+    const newar= [...new Set(arr)]
+    console.log('new '+newar)
+    
